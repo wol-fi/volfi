9ef34cba0bbd4fc61dcf594aac29c73ec9b352f9
Wolfgang Schadner <89180710+wol-fi@users.noreply.github.com>
2026-05-03 11:10:10 +0200

    Rename R API to general implied variance names

--- bindings/r/R/volfi.R
+++ bindings/r/R/volfi.R
@@ -1,5 +1,8 @@
+iv <- function(k, c) .Call(`_volfiR_iv_call_norm`, k, c)
+vol <- function(k, c, t) sqrt(iv(k, c) / t)
+iv_atm <- function(c) .Call(`_volfiR_iv_atm`, c)
+black <- function(w, h) .Call(`_volfiR_black_otm`, w, h)
 iv_otm <- function(h, c) .Call(`_volfiR_iv_otm`, h, c)
 vol_otm <- function(h, c, t) .Call(`_volfiR_vol_otm`, h, c, t)
-iv_atm <- function(c) .Call(`_volfiR_iv_atm`, c)
-iv_call_norm <- function(k, c) .Call(`_volfiR_iv_call_norm`, k, c)
-black_otm <- function(w, h) .Call(`_volfiR_black_otm`, w, h)
+iv_call_norm <- iv
+black_otm <- black
