70d02b37639671c0da651b8c6583c7260826e1e6
Wolfgang Schadner <89180710+wol-fi@users.noreply.github.com>
2026-05-03 11:31:47 +0200

    Remove OTM aliases from R interface

--- bindings/r/R/volfi.R
+++ bindings/r/R/volfi.R
@@ -1,8 +1,4 @@
-iv <- function(k, c) .Call(`_volfiR_iv_call_norm`, k, c)
+iv <- function(k, c) .Call(`_volfiR_iv`, k, c)
 vol <- function(k, c, t) sqrt(iv(k, c) / t)
 iv_atm <- function(c) .Call(`_volfiR_iv_atm`, c)
-black <- function(w, h) .Call(`_volfiR_black_otm`, w, h)
-iv_otm <- function(h, c) .Call(`_volfiR_iv_otm`, h, c)
-vol_otm <- function(h, c, t) .Call(`_volfiR_vol_otm`, h, c, t)
-iv_call_norm <- iv
-black_otm <- black
+black <- function(w, h) .Call(`_volfiR_black`, w, h)
