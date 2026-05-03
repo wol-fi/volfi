dbcb8fc76aa5e4a6f5e7d99bcc2af6e833590faf
Wolfgang Schadner <89180710+wol-fi@users.noreply.github.com>
2026-05-03 12:03:36 +0200

    Rename native R registration symbols

--- bindings/r/src/RcppExports.cpp
+++ bindings/r/src/RcppExports.cpp
@@ -1,21 +1,13 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-NumericVector iv_otm(NumericVector h, NumericVector c);
-NumericVector vol_otm(NumericVector h, NumericVector c, NumericVector t);
+NumericVector iv(NumericVector k, NumericVector c);
 NumericVector iv_atm(NumericVector c);
-NumericVector black_otm(NumericVector w, NumericVector h);
-NumericVector iv_call_norm(NumericVector k, NumericVector c);
+NumericVector black(NumericVector w, NumericVector h);
 
-RcppExport SEXP _volfiR_iv_otm(SEXP hSEXP, SEXP cSEXP){
+RcppExport SEXP _volfiR_iv(SEXP kSEXP, SEXP cSEXP){
 BEGIN_RCPP
- return wrap(iv_otm(as<NumericVector>(hSEXP),as<NumericVector>(cSEXP)));
-END_RCPP
-}
-
-RcppExport SEXP _volfiR_vol_otm(SEXP hSEXP, SEXP cSEXP, SEXP tSEXP){
-BEGIN_RCPP
- return wrap(vol_otm(as<NumericVector>(hSEXP),as<NumericVector>(cSEXP),as<NumericVector>(tSEXP)));
+ return wrap(iv(as<NumericVector>(kSEXP),as<NumericVector>(cSEXP)));
 END_RCPP
 }
 
@@ -25,24 +17,16 @@ BEGIN_RCPP
 END_RCPP
 }
 
-RcppExport SEXP _volfiR_black_otm(SEXP wSEXP, SEXP hSEXP){
+RcppExport SEXP _volfiR_black(SEXP wSEXP, SEXP hSEXP){
 BEGIN_RCPP
- return wrap(black_otm(as<NumericVector>(wSEXP),as<NumericVector>(hSEXP)));
-END_RCPP
-}
-
-RcppExport SEXP _volfiR_iv_call_norm(SEXP kSEXP, SEXP cSEXP){
-BEGIN_RCPP
- return wrap(iv_call_norm(as<NumericVector>(kSEXP),as<NumericVector>(cSEXP)));
+ return wrap(black(as<NumericVector>(wSEXP),as<NumericVector>(hSEXP)));
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
- {"_volfiR_iv_otm", (DL_FUNC) &_volfiR_iv_otm, 2},
- {"_volfiR_vol_otm", (DL_FUNC) &_volfiR_vol_otm, 3},
+ {"_volfiR_iv", (DL_FUNC) &_volfiR_iv, 2},
  {"_volfiR_iv_atm", (DL_FUNC) &_volfiR_iv_atm, 1},
- {"_volfiR_black_otm", (DL_FUNC) &_volfiR_black_otm, 2},
- {"_volfiR_iv_call_norm", (DL_FUNC) &_volfiR_iv_call_norm, 2},
+ {"_volfiR_black", (DL_FUNC) &_volfiR_black, 2},
  {NULL, NULL, 0}
 };
 
