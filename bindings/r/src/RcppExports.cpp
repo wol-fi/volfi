917896177f9c37056d350eccde705fa1d904dc4d
Wolfgang Schadner <89180710+wol-fi@users.noreply.github.com>
2026-05-03 13:29:19 +0200

    Register vol native symbol

--- bindings/r/src/RcppExports.cpp
+++ bindings/r/src/RcppExports.cpp
@@ -2,12 +2,19 @@
 using namespace Rcpp;
 
 NumericVector iv(NumericVector k, NumericVector c);
+NumericVector vol(NumericVector k, NumericVector c, NumericVector t);
 NumericVector iv_atm(NumericVector c);
 NumericVector black(NumericVector w, NumericVector h);
 
 RcppExport SEXP _volfiR_iv(SEXP kSEXP, SEXP cSEXP){
 BEGIN_RCPP
  return wrap(iv(as<NumericVector>(kSEXP),as<NumericVector>(cSEXP)));
+END_RCPP
+}
+
+RcppExport SEXP _volfiR_vol(SEXP kSEXP, SEXP cSEXP, SEXP tSEXP){
+BEGIN_RCPP
+ return wrap(vol(as<NumericVector>(kSEXP),as<NumericVector>(cSEXP),as<NumericVector>(tSEXP)));
 END_RCPP
 }
 
@@ -25,6 +32,7 @@ END_RCPP
 
 static const R_CallMethodDef CallEntries[] = {
  {"_volfiR_iv", (DL_FUNC) &_volfiR_iv, 2},
+ {"_volfiR_vol", (DL_FUNC) &_volfiR_vol, 3},
  {"_volfiR_iv_atm", (DL_FUNC) &_volfiR_iv_atm, 1},
  {"_volfiR_black", (DL_FUNC) &_volfiR_black, 2},
  {NULL, NULL, 0}
