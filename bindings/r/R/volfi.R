294a57aeb82d911e0706c7131e79b775f980dcd0
Wolfgang Schadner <89180710+wol-fi@users.noreply.github.com>
2026-05-03 13:41:36 +0200

    r cleanup

--- bindings/r/R/volfi.R
+++
@@ -1,4 +0,0 @@
-iv <- function(k, c) .Call(`_volfiR_iv`, k, c)
-vol <- function(k, c, t) .Call(`_volfiR_vol`, k, c, t)
-iv_atm <- function(c) .Call(`_volfiR_iv_atm`, c)
-black <- function(w, h) .Call(`_volfiR_black`, w, h)
